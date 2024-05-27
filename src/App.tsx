f9108bf489c951880d6b8714a0c279d268438b9e
Leonardo Montini <lion.48m@gmail.com>
2024-05-27 14:14:59 +0200

    prer for next chapter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Typography from "./components/ui/typography";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-background flex flex-col items-center">
+    <div className="w-full h-full bg-background flex flex-col items-center">
       <Typography
         variant={"h1"}
         className="my-6 text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-yellow-600"
--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const typographyVariants = cva("text-xl", {
   variants: {
     variant: {
-      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg: text-5xl",
-      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+      h2: "scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0",
       h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h4: "scroll-m-20 text-xl font-semibold tracking-tight",
       p: "leading-7 [&:not(:first-child)]:mt-6",
